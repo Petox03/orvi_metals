a1e27178cbd69f0d1867a077233a56eef094784c
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-08-27 13:10:56 -0600

    fix: se simplificó el componente ProductFeatures para prevención de errores y/o difucultades en el futuro envío de datos

--- components/ProductComponents/ProductFeatures.jsx
+++ components/ProductComponents/ProductFeatures.jsx
@@ -1,9 +1,23 @@
 'use client';
 import { RadioGroup, Radio } from "@nextui-org/radio";
-import { Counter } from "@/components/ProductComponents/counter";
+import { useState } from "react";
 import { Button } from "@nextui-org/button";
 
 export default function ProductFeatures() {
+
+    const [count, setCount] = useState(0);
+
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+
     return (
         <section className="mt-12 text-center md:text-start">
             <p>Tipo</p>
@@ -17,7 +31,16 @@ export default function ProductFeatures() {
 
             <div className="mt-7 items-end flex flex-row">
                 <Button className="mr-5 md:w-60" color="primary" size="lg" radius="none">Agregar Al Carrito</Button>
-                <Counter></Counter>
+
+                {/* counter */}
+                <div className="items-center">
+                    <h1 className="text-sm">Cantidad</h1>
+                    <div className="flex gap-4 border-2 items-center">
+                        <Button size="sm" className="bg-transparent hover:bg-zinc-300 dark:hover:bg-zinc-700 text-xl px-4 py-2 rounded" onClick={decrement}>-</Button>
+                        <span className="text-xl">{count}</span>
+                        <Button size="sm" className="bg-transparent hover:bg-zinc-300 dark:hover:bg-zinc-700 text-xl px-4 py-2 rounded" onClick={increment}>+</Button>
+                    </div>
+                </div>
             </div>
 
         </section>
