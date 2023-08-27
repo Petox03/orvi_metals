ca0f295ec62aa3eff5488d9953773bac40d1135f
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-08-27 16:03:37 -0600

    style(login): Cambio de colores en el form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     return (
 
         <div className="relative md:static grid grid-cols-1 md:grid-cols-3 justify-items-center">
-            <div className="z-10 p-5 | absolute md:static | container min-h-screen | grid justify-items-center items-center bg-white dark:bg-slate-800">
+            <div className="z-10 p-5 | absolute md:static | container min-h-screen | grid justify-items-center items-center bg-white dark:bg-[#0c0c0c]">
                 <LogInComponent/>
             </div>
             <div className="z-0 absolute md:static container col-span-2 bg-repeat bg-center min-h-screen" style={{ backgroundImage: "url('img/orvimetals.jpg')" }}>
