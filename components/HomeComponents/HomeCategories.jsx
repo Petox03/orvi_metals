d555965d985739a902b9e81397e09e53dce31b8f
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-08-27 16:08:38 -0600

    style(Home <categories component>): Cambio de tamaños y en las categorías y en colores del separador

--- components/HomeComponents/HomeCategories.jsx
+++ components/HomeComponents/HomeCategories.jsx
@@ -8,34 +8,33 @@ export default function HomeCategories() {
         <>
 
             <div className="flex mt-12 min-w-screen justify-center items-center">
-                <p className="text-3xl md:text-4xl font-light">Categorías principales</p>
+                <p className="text-3xl md:text-5xl font-light">Categorías principales</p>
             </div>
 
-            <div className="container relative | grid grid-cols-1 md:grid-cols-2 gap-20 gap-y-10 | mt-10 | h-[30rem] items-center">
+            <div className="container relative | grid grid-cols-1 md:grid-cols-2 gap-20 gap-y-10 | mt-10 | h-[35rem] items-center">
 
                 <Card className="h-full">
                     <CardBody className="items-center justify-center">
-                        <p className="text-2xl font-normal">Refractarios</p>
+                        <p className="text-4xl font-light">Refractarios</p>
                     </CardBody>
                 </Card>
                 <Card className="h-full">
                     <CardBody className="items-center justify-center">
-                        <p className="text-2xl font-normal">Refractarios</p>
+                        <p className="text-4xl font-light">Refractarios</p>
                     </CardBody>
                 </Card>
                 <Card className="h-full">
                     <CardBody className="items-center justify-center">
-                        <p className="text-2xl font-normal">Refractarios</p>
+                        <p className="text-4xl font-light">Refractarios</p>
                     </CardBody>
                 </Card>
                 <Card className="h-full">
                     <CardBody className="items-center justify-center">
-                        <p className="text-2xl font-normal">Refractarios</p>
+                        <p className="text-4xl font-light">Refractarios</p>
                     </CardBody>
                 </Card>
 
-                <Divider className="absolute hidden md:block left-[50%]" orientation="vertical"></Divider>
-
+                <Divider className="absolute w-0.5 hidden md:block left-[50%] bg-[#323232] dark:bg-[#262626]" orientation="vertical"></Divider>
             </div>
         </>
     );
