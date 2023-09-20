a785d0a8f9f06d785e01232fa78922e9e69ec00a
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-09-20 10:30:59 -0600

    style(home): Se arregló el duplicado de la imagen de syncron

--- components/HomeComponents/HomeCategories.jsx
+++ components/HomeComponents/HomeCategories.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { Card, CardBody } from "@nextui-org/card";
 import Link from "next/link";
+import Image from "next/image";
 import { Divider } from "@nextui-org/divider";
 
 export default function HomeCategories() {
@@ -28,10 +29,9 @@ export default function HomeCategories() {
                         <p className="text-4xl font-light text-slate-50">Metales</p>
                     </CardBody>
                 </Card>
-                <Card className="h-full bg-cover bg-no-repeat bg-center" style={{ background: "url('img/syncron.jpg')" }}>
-                    <CardBody className="items-center justify-center">
-                        <p className="text-4xl font-light">Lubricantes</p>
-                    </CardBody>
+                <Card className="h-full relative">
+                    <Image src={"/img/syncron.jpg"} alt="syncron image" width={1800} height={1800} className="w-full h-full"></Image>
+                        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-light text-slate-300">Lubricantes</p>
                 </Card>
 
                 <Divider className="absolute w-0.5 hidden md:block left-[50%] bg-[#323232]" orientation="vertical"></Divider>
